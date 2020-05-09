d214b492525f2533b53e164928d51e4476ba97a3
Arindam Aluni <aaluni@gmail.com>
2020-05-09 11:30:56 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring5-mongo-recipe-app
+Sample Spring boot Recipe Application Using MongoDB - Credit to Spring Faramework Guru
+
+
+
---
+++ src/main/java/aaluni/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,13 @@
+package aaluni.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class UnitOfMeasure {
+
+    private String id;
+    private String description;
+}

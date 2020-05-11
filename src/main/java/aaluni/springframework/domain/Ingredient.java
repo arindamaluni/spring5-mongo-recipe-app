95ef5ff544c02b2a41e64d6809c40b68ce43ff83
Arindam Aluni <aaluni@gmail.com>
2020-05-11 11:27:03 +0100

    In memory Mongo DB connection, Entity pojos,

--- src/main/java/aaluni/springframework/domain/Ingredient.java
+++ src/main/java/aaluni/springframework/domain/Ingredient.java
@@ -2,19 +2,24 @@ package aaluni.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
+import java.util.UUID;
+
 
 @Getter
 @Setter
 public class Ingredient {
-
-    private String id;
+	
+	//Mongo does not generate ID for embedded lists so @Id wont work
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
-    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -29,7 +34,7 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        this.recipe = recipe;
+       // this.recipe = recipe;
     }
 
 }
--- src/main/java/aaluni/springframework/domain/Notes.java
+++ src/main/java/aaluni/springframework/domain/Notes.java
@@ -2,14 +2,14 @@ package aaluni.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 
 @Getter
 @Setter
 public class Notes {
 
+    @Id
     private String id;
-    private Recipe recipe;
     private String recipeNotes;
-
 }
